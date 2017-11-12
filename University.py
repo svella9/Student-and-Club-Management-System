37e6ea9143bf976da8442d3d86f69a0c0646c437
svella9 <svella9@gmail.com>
2017-11-12 15:53:09 +0530

    first commit

---
+++ University.py
@@ -0,0 +1,103 @@
+#UNIVERSITY DATABASE
+#Define SCHEMA of the DATABASE
+from app import db
+from datetime import datetime
+
+class Student(db.Model):
+	usn = db.Column(db.String(12), primary_key = True)
+	name = db.Column(db.String(25))
+	sem = db.Column(db.Integer)
+	dept = db.Column(db.String(3))
+	email = db.Column(db.String(50), unique = True)
+	mob = db.Column(db.BigInteger, unique = True)
+
+	#foreign key relationship
+	#backref: creates a virtual column called 'student' in 'Student_feedback' class that references the 'Student' class.
+	feedback_by_student = db.relationship('Student_feedback' , backref = 'student', lazy = 'dynamic')
+	feedback_by_faculty = db.relationship('Faculty_feedback', backref = 'student', lazy = 'dynamic')
+	student_n_advisor = db.relationship('Student_and_advisor', backref = 'student', lazy = 'dynamic')
+	
+	def __init__(self, usn, name, sem, dept, email, mob):
+		self.usn = usn
+		self.name = name
+		self.sem = sem
+		self.dept = dept
+		self.email = email
+		self.mob = mob
+
+	def __repr__(self):
+		return '<Student %r>' % self.name
+
+class Faculty(db.Model):
+	fid = db.Column(db.String(12), primary_key = True)
+	name = db.Column(db.String(25))
+	dept = db.Column(db.String(3))
+	email = db.Column(db.String(50), unique = True)
+	mob = db.Column(db.BigInteger, unique = True)
+
+	feedback_by_faculty = db.relationship('Faculty_feedback', backref = 'faculty', lazy = 'dynamic')
+	faculty_advisor = db.relationship('Student_and_advisor', backref = 'faculty', lazy = 'dynamic' )
+
+	def __init__(self, fid, name, dept, email, mob):
+		self.fid = fid
+		self.name = name
+		self.dept = dept
+		self.email = email
+		self.mob = mob
+
+	def __repr__(self):
+		return '<Faculty %r>' %self.name
+
+class Student_credential(db.Model):
+	usn = db.Column(db.String(12) , primary_key = True)
+	password = db.Column(db.String(50))
+
+	def __init__(self, usn, password):
+		self.usn = usn
+		self.password = password
+
+class Faculty_credential(db.Model):
+	fid = db.Column(db.String(12), primary_key = True)
+	password = db.Column(db.String(50), primary_key = True)
+
+	def __init__(self, fid, password):
+		self.fid = fid
+		self.password = password
+
+class Student_feedback(db.Model):
+	id = db.Column(db.Integer , primary_key = True)
+	usn = db.Column(db.String(12) , db.ForeignKey('student.usn'))
+	#Record the current date and time when the row is inserted
+	date = db.Column(db.DateTime() , default = datetime.now())
+	feedback = db.Column(db.String(200))
+
+	def __init__(self, feedback, student):
+		self.feedback = feedback
+		self.student = student
+
+class Faculty_feedback(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	fid = db.Column(db.String(12), db.ForeignKey('faculty.fid'))
+	student_usn = db.Column(db.String(12), db.ForeignKey('student.usn'))
+	#Record the current date and time when the row is inserted
+	date = db.Column(db.DateTime() , default = datetime.now())
+	feedback = db.Column(db.String(200))
+
+	def __init__(self, feedback, faculty, student):
+		self.feedback = feedback
+		self.faculty = faculty
+		self.student = student
+
+
+class Student_and_advisor(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	usn = db.Column(db.String(12), db.ForeignKey('student.usn'), unique = True)
+	fid = db.Column(db.String(12), db.ForeignKey('faculty.fid'))
+
+	def __init__(self, usn, fid):
+		self.usn = usn
+		self.fid = fid
+		self.student = student
+		self.faculty = faculty
+
+db.create_all()
---
+++ app.py
@@ -0,0 +1,205 @@
+from flask import Flask, flash, request, abort, render_template
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.exc
+from sqlalchemy import and_, or_
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mypassword@localhost/University' #URI format: 'postgres://username:password@localhost/database_name'
+db = SQLAlchemy(app)
+
+mail_credentials = open('credentials.txt').read().split('\n')
+#print(mail_credentials)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = mail_credentials[0]
+app.config['MAIL_PASSWORD'] = mail_credentials[1]
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+
+@app.route('/')
+def hello():
+	#return "Hello World!!"
+	return render_template('home.html')
+
+@app.route('/Student/register/', methods = ['POST'])
+def student_register():
+	from University import Student, Faculty, Student_credential, Faculty_credential
+	try:
+		if request.method == 'POST':
+			usn = request.form['usn']
+			name = request.form['name']
+			sem = request.form['sem']
+			dept = request.form['dept']
+			email = request.form['email']
+			mob = request.form['mob']
+			password = request.form['password']
+
+			#Create a student object to insert into the Student table
+			student = Student(usn, name, sem, dept, email, mob)
+			student_credential = Student_credential(usn, password)
+
+			db.session.add(student)
+			db.session.add(student_credential)
+			db.session.commit()
+
+			return 'Student Record added successfully!!'
+
+		else:
+			#Bad Request
+			abort(400)
+
+	except sqlalchemy.exc.IntergrityError:
+		db.session.rollback()
+		return "Account already exists.."
+
+	except:
+		db.session.rollback()
+		return "Cannot register user..."
+
+@app.route('/Faculty/register/', methods = ['POST'])
+def faculty_register():
+	from University import Student, Faculty, Student_credential, Faculty_credential
+
+	try:
+		if request.method == 'POST':
+			fid = request.form['fid']
+			name = request.form['name']
+			dept = request.form['dept']
+			email = request.form['email']
+			mob = request.form['mob']
+			password = request.form['password']
+
+			#Create a faculty object to insert into the Faculty table
+			faculty = Faculty(fid, name, dept, email, mob)
+			faculty_credential = Faculty_credential(fid, password)
+
+			db.session.add(faculty)
+			db.session.add(faculty_credential)
+			db.session.commit()
+
+			return 'Faculty Record added successfully!!'
+
+		else:
+			#Bad Request
+			abort(400)
+	except sqlalchemy.exc.IntergrityError:
+		db.session.rollback()
+		return "Account already exists..."
+	except:
+		db.session.rollback()
+		return "Cannot register..."
+
+
+@app.route('/notify/<int:semester>/')
+def send_notification(semester):
+	"""Send notification to all students belonging to a particular semester"""
+
+	from University import Student
+	#query to retrieve students
+	students = Student.query.filter_by(sem = semester).all()
+	#keep only the email-id of the students
+	students = list(map(lambda x : x.email, students))
+	#print('sending Message', students)
+	msg = Message('Meeting Schedule Notification.',
+			sender = 'pesfacultyadvisor.sepro2017@gmail.com',
+			recipients = students)
+	#print('Object created!')
+	msg.body = "Dear Student\n A meeting is scheduled on so and so date.\n We request you to attend the meeting."
+	mail.send(msg)
+
+	return "Notification Sent!!"
+
+@app.route('/Student/savefeedback/', methods = ['POST'])
+def student_save_feedback():
+	"""Save the feedback/issues given by the student in the database"""
+	from University import Student, Student_feedback
+	try:
+		if request.method == 'POST':
+			usn = request.form['usn']
+			feedback = request.form['feedback']
+
+			#get the student object from the table bearing the usn
+			q = Student.query.filter_by(usn = usn).first()
+			#pass the student object to the Student_feedback class for the purpose of foreign key establishment.
+			student_feedback = Student_feedback(feedback = feedback, student = q)
+			db.session.add(student_feedback)
+			db.session.commit()
+			return "Feedback Submitted successfully!"
+
+		else:
+			abort(400)
+	except:
+		db.session.rollback()
+		return "Cannot save feedback..."
+
+@app.route('/Faculty/savefeedback/', methods = ['POST'])
+def faculty_save_feedback():
+	"""Save the feedback given by the faculty for a student in the database"""
+	from University import Student, Faculty, Faculty_feedback
+	try:
+		if request.method == 'POST':
+			fid = request.form['fid']
+			student_usn = request.form['student_usn']
+			feedback = request.form['feedback']
+
+			#get the faculty object bearing the fid
+			faculty = Faculty.query.filter_by(fid = fid).first()
+			#get the student object bearing the usn
+			student = Student.query.filter_by(usn = student_usn).first()
+			#pass the faculty and student object to the Faculty_feedback class for the purpose of foreign key establishment.
+			faculty_feedback = Faculty_feedback(feedback = feedback, faculty = faculty, student = student)
+			db.session.add(faculty_feedback)
+			db.session.commit()
+			return "Your feedback is submitted!"
+
+		else:
+			abort(400)
+	except:
+		db.session.rollback()
+		return "Cannot save feedback..."
+
+
+@app.route('/Student/login/', methods = ['POST'])
+def student_login():
+	from University import Student, Student_credential
+	try:
+		if request.method == 'POST':
+			usn = request.form['usn']
+			password = request.form['password']
+
+			obj = Student_credential.query.filter(and_(Student_credential.usn == usn, Student_credential.password == password)).first()
+			if obj != None:
+				return "Sucess"
+			else:
+				return "Enter correct usn/password..."
+	except:
+		return "Error.."
+
+@app.route('/Faculty/login/', methods = ['POST'])
+def faculty_login():
+	from University import Faculty, Faculty_credential
+	try:
+		if request.method == 'POST':
+			fid = request.form['fid']
+			password = request.form['password']
+
+			obj = Faculty_credential.query.filter(and_(Faculty_credential.fid == fid , Faculty_credential.password == password)).first()
+			if obj != None:
+				#return redirect(url_for('faculty_home', fid = fid))
+				return "Success.."
+			else:
+				return "Enter correct fid/password..."
+	except:
+		return "Error..."
+
+@app.route('/Faculty/home/<int:fid>/')
+def faculty_home(fid):
+	from University import Student, Faculty, Student_and_advisor
+	try:
+		q = Student_and_advisor.query.filter_by(fid = fid).all()
+		return render_template('facultyHomepage.html', advisors_students = q)
+	except:
+		pass
