2917cb4dcce1c7f083ce2cf99f996aa769c0019b
Rakshith Gowda VR <rrrakshit10@gmail.com>
2017-11-17 21:10:56 +0530

    New templates are added

--- ClubManagement/app.py
+++ ClubManagement/app.py
@@ -17,12 +17,7 @@ def addClubToDatabase():
 	if request.method == 'POST':
 		name = request.form['name']
 		id = request.form['id']
-		print(name)
-		print()
-		print()
-		print(id)
 		club = Club(name,id)
-		print(club)
 		db.session.add(club)
 		db.session.commit()
 
@@ -36,33 +31,7 @@ def addClubToDatabase():
 @app.route('/addNewMember')
 def addNewMember():
 	return render_template('memberInformation.html')
-  
-@app.route('/Student/register', methods = ['POST'])
-def student_register():
-	from database import Member,Event_Volenteers,Attendance
-	
-	if request.method == 'POST':
-		name = request.form['name']
-		email = request.form['email']
 		
-		member = Member(name,email)
-		
-		db.session.add(Member)
-		db.session.commit()
-
-		return 'Member added successfully!!'
-
-	else:
-		#Bad Request
-		abort(400)
-		
-@app.route('/listAllMembers')
-def list():
-	return render_template("listAllMembers.html",member=Member.query.all())
-
-@app.route('/addVolunterForAnEvent')
-def addVolunterForAnEvent():
-	return render_template('VolunterInformation.html')
 
 @app.route('/addMemberToDatabase',methods = ['POST'])
 def addMemberToDatabase():   #member,event,event_Volenteers,Attendance
@@ -82,35 +51,87 @@ def addMemberToDatabase():   #member,event,event_Volenteers,Attendance
 		#Bad Request
 		abort(400)
 
+@app.route('/listAllMembers')
+def list():
+	from database import Member
+	return render_template("listAllMembers.html",rows=Member.query.all())
+
+@app.route('/addNewEvent')
+def addNewEvent():
+	return render_template('eventInformation.html')
+
+@app.route('/addEventToDatabase',methods = ['POST'])
+def addEventToDatabase():   #member,event,event_Volenteers,Attendance
+	from database import Event
+	if request.method == 'POST':
+		name = request.form['name']
+		eventId = request.form['eventId']
+		clubId = request.form['clubId']
+		event=Event(eventId,name,clubId)
+		db.session.add(event)
+		db.session.commit()
+
+		return 'Event added successfully!!'
+
+	else:
+		#Bad Request
+		abort(400)
+
+@app.route('/listAllEvents')
+def listEvents():
+	from database import Event
+	return render_template("listAllEvents.html",rows=Event.query.all())
+
+@app.route('/addVolunterForAnEvent')
+def addVolunterForAnEvent():
+	return render_template('VolunterInformation.html')
+
+@app.route('/addVolunterToDatabase',methods = ['POST'])
+def addVolunterToDatabase():   #member,event,event_Volenteers,Attendance
+	from database import Event_Volenteers
+	if request.method == 'POST':
+		usn = request.form['usn']
+		eventId = request.form['eventId']
+		event_volenteers=Event_Volenteers(usn,eventId)
+		db.session.add(event_volenteers)
+		db.session.commit()
+
+		return 'Event Volunteer added successfully!!'
+
+	else:
+		#Bad Request
+		abort(400)
+
+@app.route('/listAllVolunteers')
+def listAllVolunteers():	
+	from database import Event_Volenteers
+	return render_template("listAllVolunteers.html",rows = Event_Volenteers.query.all())
+
 @app.route('/markAttendanceOfAnEvent')
 def markAttendanceOfAnEvent():
 	return render_template('attendanceInformation.html')
 
 @app.route('/markAttendanceToDatabase',methods = ['POST'])
 def markAttendanceToDatabase():		
-	from database import Member,Event,Event_Volenteers,Attendance
+	from database import Attendance
 	
 	if request.method == 'POST':
-		name = request.form['name']
-		eName = request.form['eventName']
+		eventId = request.form['eventId']
+		usn = request.form['usn']
 		date = request.form['date']
-		attendance=Attendance(name,ename,head)
-		
+		attendance=Attendance(eventId,usn,date)
 		db.session.add(attendance)
 		db.session.commit()
 
-		return 'attendance added'
+		return 'attendance marked'
 	else:
 		#Bad Request
-		abort(400)	
-		
-@app.route('/listAllVolunters')
-def listAllVolunters():	
-	return render_template("listAllVolunters.html",volenteer.query.all())	
+		abort(400)		
 
 @app.route('/listAllAttendance')
 def listAllAttendance():
-	return render_template("listAllVolunters.html",attendance.query.all())  
+	from database import Attendance
+	return render_template("listAllAttendance.html",rows=Attendance.query.all())  
 
 if __name__ == '__main__':
 	app.run(debug=True)		
--- ClubManagement/database.py
+++ ClubManagement/database.py
@@ -1,5 +1,24 @@
 from app import db
 
+class Event_Volenteers(db.Model):
+	#id = db.Column(db.Integer,primary_key=True)
+	usn = db.Column(db.String(10),db.ForeignKey('member.usn'), primary_key = True)
+	eventId= db.Column(db.String(10),db.ForeignKey('event.eventId'), primary_key = True)	
+	def __init__(self,usn,eventId):
+		self.usn = usn
+		self.eventId = eventId				
+
+class Attendance(db.Model):
+	#id = db.Column(db.Integer,primary_key = True)
+	eventId= db.Column(db.String(10),db.ForeignKey('event.eventId'),primary_key = True)	
+	usn = db.Column(db.String(10),db.ForeignKey('member.usn'),primary_key = True)
+	date= db.column(db.String(20))
+	def __init__(self,eventId,usn,date):
+		self.usn = usn
+		self.eventId = eventId
+		self.date = date		
+
+
 class Club(db.Model):
 	name = db.Column(db.String(50))
 	id = db.Column(db.String(10), primary_key = True)
@@ -10,11 +29,12 @@ class Club(db.Model):
 		self.id = id
 
 class Member(db.Model):
-	usn = db.Column(db.String(10), primary_key=True)
+	usn = db.Column(db.String(10),primary_key=True)
+	name = db.Column(db.String(20))
 	clubId = db.Column(db.String(10), db.ForeignKey('club.id'))
 	email = db.Column(db.String(25))
-	eventVolenteer_constraint = db.relationship('Event_Volenteers', backref='eventVolenteers',lazy='dynamic')
-	attendance_constraint = db.relationship('Attendance', backref='attendance',lazy='dynamic')
+	eventVolenteer_constraint = db.relationship('Event_Volenteers', backref='eventVolenteers2',lazy='dynamic')
+	attendance_constraint = db.relationship('Attendance', backref='attendance2',lazy='dynamic')
 	def __init__(self,cId,name,usn,email):
 		self.usn = usn
 		self.name = name
@@ -25,29 +45,11 @@ class Event(db.Model):
 	eventId = db.Column(db.String(10), primary_key = True)
 	name = db.Column(db.String(25))	
 	clubId = db.Column(db.String(10), db.ForeignKey('club.id'))
-	eventVolenteer_constraint = db.relationship('Event_Volenteers', backref='eventVolenteers',lazy='dynamic')	
-	attendance_constraint = db.relationship('Attendance', backref='attendance',lazy='dynamic')	
+	eventVolenteer_constraint = db.relationship('Event_Volenteers', backref='eventVolenteers1',lazy='dynamic')	
+	attendance_constraint = db.relationship('Attendance', backref='attendance1',lazy='dynamic')	
 	def __init__(self,eventId,name,cId):
 		self.name = name
 		self.clubId= cId
 		self.eventId= eventId
-	
-class Event_Volenteers(db.Model):
-	usn = db.Column(db.String(10),db.ForeignKey('member.usn'),primary_key=True)
-	eventId= db.Column(db.String(10),db.ForeignKey('event.eventId'),primary_key=True)	
-	def __init__(self,usn,eventId):
-		self.usn = usn
-		self.eventId = eventId				
 
-class Attendance(db.Model):
-	#id = db.Column(db.Integer,primary_key = True)
-	eventId= db.Column(db.String(10),db.ForeignKey('event.eventId'),primary_key = True)	
-	usn = db.Column(db.String(10),db.ForeignKey('member.usn'),primary_key = True)
-	date= db.column(db.String(20))
-	def __init__(self,event,name,date):
-		self.name=name
-		self.event= event
-		self.date=date		
-	
 db.create_all()		
-			
