c6145235f43d543582bcdef57e73f2a7cd34c304
svella9 <svella9@gmail.com>
2017-11-22 20:04:17 +0530

    Added Styling to facultyFeedbackpage

--- deleteRows.py
+++ deleteRows.py
@@ -4,20 +4,45 @@ from University import Student, Student_credential, Faculty, Faculty_credential,
 for obj in Student.query.all():
 	db.session.delete(obj)
 
+
+print('Student Done.')
+db.session.commit()
+print('Done')
+
 for obj in Student_credential.query.all():
 	db.session.delete(obj)
+
+print('Student Credential Done.')
+db.session.commit()
+print('Done')
 
 for obj in Faculty.query.all():
 	db.session.delete(obj)
 
+print('Faculty Done.')
+db.session.commit()
+print('Done')
+
 for obj in Faculty_credential.query.all():
-		db.session.delete(obj)
+	db.session.delete(obj)
+
+print('Faculty_credential Done.')
+db.session.commit()
+print('Done')
 
 for obj in Student_feedback.query.all():
 	db.session.delete(obj)
 
+print('Student_feedback Done.')
+db.session.commit()
+print('Done')
+
 for obj in Faculty_feedback.query.all():
 	db.session.delete(obj)
+
+print('Faculty_feedback Done.')
+db.session.commit()
+print('Done')
 
 for obj in Student_and_advisor.query.all():
 	db.session.delete(obj)
